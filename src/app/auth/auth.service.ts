dd73689747a0cf4bf6a04914e19637fc46727e40
Kurt Metulj <kurt.metulj@gmail.com>
2017-12-13 14:56:39 +0100

    Build project skeleton

---
+++ src/app/auth/auth.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import { BehaviorSubject } from 'rxjs/BehaviorSubject';
+import { AUTH_CONFIG } from './auth.config';
+import * as auth0 from 'auth0-js';
+
+@Injectable()
+export class AuthService {
+
+  // Create Auth0 web auth instance
+
+  constructor() { }
+
+}
