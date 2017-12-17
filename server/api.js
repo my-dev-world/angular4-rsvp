a96f3f54c58556ada6714bc8436ecb9d8a8f4e3b
Kurt Metulj <kurt.metulj@gmail.com>
2017-12-17 23:04:59 +0100

    Create API endpoints

--- server/api.js
+++ server/api.js
@@ -50,6 +50,72 @@ module.exports = function(app, config) {
   // GET API root
   app.get('/api/', (req, res) => {
     res.send('API works');
-});
+  });
 
+  const _eventListProjection = 'title startDatetime endDatetime viewPublic';
+
+  // Get list of public events starting in the future
+  app.get('/api/events', (req, res) => {
+    Event.find({viewPublic: true, startDatetime: { $gte: new Date() }}, _eventListProjection, (err, events) => {
+      let eventsArr = [];
+      if (err) {
+        return res.status(500).send({message: err.message});
+      }
+      if (events) {
+        events.forEach(event => {
+          eventsArr.push(event);
+        })
+      }
+
+      res.send(eventsArr);
+    });
+  });
+
+  // Get list of all api events, public and private (admin only)
+  app.get('/api/events/admin', jwtCheck, adminCheck, (req, res) => {
+    Event.find({}, _eventListProjection, (err, events) => {
+      let eventsArr = [];
+      if (err) {
+        return res.status(500).send({message: err.message});
+      }
+      if (events) {
+        events.forEach(event => {
+          eventsArr.push(event);
+        })
+      }
+
+      res.send(eventsArr);
+    });
+  });
+
+  // Get event by event ID
+  app.get('/api/events/:id', jwtCheck, (req, res) => {
+    Event.findById(req.params.id, (err, event) => {
+      if (err) {
+        return res.status(500).send({message: err.message});
+      }
+      if (!event) {
+        return res.status(500).send({message: 'Event not found'});
+      }
+
+      res.send(event);
+    });
+  });
+
+  // GET RSVPs by event ID
+  app.get('/api/event/:eventId/rsvps', jwtCheck, (req, res) => {
+    Rsvp.find({eventId: req.params.eventId}, (err, rsvps) => {
+      let rsvpArr = [];
+      if (err) {
+        return res.status(500).send({message: err.message});
+      }
+      if (rsvps) {
+        rsvps.forEach(rsvp => {
+          rsvpArr.push(rsvp);
+        });
+      }
+
+      res.send(rsvpArr);
+    });
+  });
 };
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,16 @@
 import { BrowserModule, Title } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { CallbackComponent } from './pages/callback/callback.component';
 
 import { AuthService} from './auth/auth.service';
-import { CallbackComponent } from './pages/callback/callback.component';
+import { ApiService } from './core/api.service';
 
 @NgModule({
   declarations: [
@@ -20,11 +22,13 @@ import { CallbackComponent } from './pages/callback/callback.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [
     Title,
-    AuthService
+    AuthService,
+    ApiService
   ],
   bootstrap: [AppComponent]
 })
